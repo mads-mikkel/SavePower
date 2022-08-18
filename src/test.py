14399869a4fd6da2227c191008003afeb8aa7f36
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-08-18 15:09:49 +0200

    Extract and transform test complete: success

--- src/test.py
+++ src/test.py
@@ -1,14 +1,34 @@
 from urllib.request import urlopen
-
+from datetime import datetime
 import json
 
-url = "https://api.energidataservice.dk/dataset/Elspotprices?limit=100&offset=0&start=2022-08-18T00:00&end=2022-08-19T00:00&filter=%7B%22PriceArea%22:%22DK1%22%7D&sort=HourDK%20DESC&timezone=dk"
+class HourPrice(object):
+    def __init__(self, date, hour, price):
+        self.date = date,
+        self.hour = hour,
+        self.price = price
 
-print("Henter data..."),
+    def __str__(self): 
+        return "{0} {1} {2}".format(self.date, self.hour, self.price)
 
-response = urlopen(url)
+def getData():
+    url = "https://api.energidataservice.dk/dataset/Elspotprices?limit=100&offset=0&start=2022-08-18T00:00&end=2022-08-19T00:00&filter=%7B%22PriceArea%22:%22DK1%22%7D&sort=HourDK%20DESC&timezone=dk"
+    #response = urlopen(url)
+    response = open(r"C:\Users\mara\source\repos\SavePower\src\example.json")
+    jsonData = json.loads(response.read())
+    response.close()    #test file close
+    return jsonData
 
-json = json.loads(response.read())
+def getPowerPrices(jsonData):
+    prices = []
+    for entry in jsonData['records']:
+        date = entry['HourDK'][0:10]
+        hour = entry['HourDK'][11:13]
+        price = round(entry['SpotPriceDKK'])
+        print(f"{date} kl. {hour}: {price} øre/kWh")
 
-print("Data hentet:")
+
+print("henter data...")
+json = getData()
 print(json)
+getPowerPrices(json)
